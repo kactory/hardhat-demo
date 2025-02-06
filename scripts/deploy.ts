d35b34b581b96efa2f2abf75ae6355e87b004736
keykat <koin20000@gmail.com>
2025-02-06 10:41:32 +0900

    [feature] mint해보기

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,18 @@
 import { ethers } from "hardhat";
 
 async function main() {
+    // await deploy();
+    await mint();
+
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
+
+/// 토큰 스마트 컨트랙트. 토큰의 개념을 발행한다고 보면 됨.
+async function deploy() {
     // KEYKAT이라는 토큰의 "개념", 즉 토큰의 스마트 컨트랙트를 배포
     // KEYKAT이라는 이름의 토큰 컨트랙트를 이더리움 네트워크에 배포.
     const keykatFactory = await ethers.getContractFactory("KEYKAT");
@@ -12,7 +24,35 @@ async function main() {
     console.log("Token deployed to:", keykat.target);
 }
 
-main().catch((error) => {
+deploy().catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
+
+/// 토큰 발행
+async function mint() {
+    // getSigners(): 이더리움 네트워크에 연결된 계정 목록 (테스트할 때 많이 사용)
+    const [deployer] = await ethers.getSigners();
+    // 토큰의 컨트랙트 주소 (TODO: 이거 어떻게 관리함?)
+    const tokenAddress = "0xecc81F04Fdf99eD9905bcda035C63a0856d6D317";
+    // 토큰의 컨트랙트 인스턴스
+    const Keykat = await ethers.getContractAt("KEYKAT", tokenAddress);
+
+    const amount = ethers.parseUnits("500", 18);
+
+    // 토큰 발행
+    // 1. 이거 gas fee 얼마로 설정해야함? 너무 낮으면 잘 안됨
+    // 2. transaction can't be replaced, mining has already started. 
+    //    한번만 실행했는데 이 에러가 뜨는데 왜 무조건 발생할까?
+    const tx = await Keykat.mint(deployer.address, amount, {
+        gasPrice: ethers.parseUnits('200000000000', 'wei'),
+    });
+    await tx.wait();
+
+    console.log("Minted", amount, "KEYKAT to", deployer.address);
+}
+
+mint().catch((error) => {
     console.error(error);
     process.exit(1);
 });
